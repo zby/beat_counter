767678d8a1a5cd1ee14a3531c757e6c50fd67174
Zbigniew Lukasiak <zzbbyy@gmail.com>
2025-03-23 21:37:08 +0100

    installation improvements

--- beat_detection/__init__.py
+++ beat_detection/__init__.py
@@ -2,3 +2,9 @@
 Beat Detection - A package for detecting beats in music and adding metronome sounds.
 """
 __version__ = "0.1.0"
+
+from . import core
+from . import utils
+from . import cli
+
+__all__ = ['core', 'utils', 'cli']
